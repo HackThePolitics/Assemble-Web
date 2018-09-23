667584d4204e5866e0c0bc8fa61f9ce63c3f947d
Jie <j42lin@users.noreply.github.com>
2018-09-23 09:14:44 -0400

    ready for release

---
+++ src/config/firebase.js
@@ -0,0 +1,10 @@
+var config = {
+  apiKey: "AIzaSyDjOem3FWI0oY1TCTWXETWgn6WQizWUFxU",
+  authDomain: "elevate-preview.firebaseapp.com",
+  databaseURL: "https://elevate-preview.firebaseio.com",
+  projectId: "elevate-preview",
+  storageBucket: "elevate-preview.appspot.com",
+  messagingSenderId: "990321701432"
+};
+
+export default config;
