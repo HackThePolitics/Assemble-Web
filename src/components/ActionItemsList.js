3bf5dc9316cc376348f17d4bb0814790472d0b76
TSL016 <mweeks@tribalscale.com>
2018-09-22 22:48:31 -0400

    feat(solutions): Add ActionItemsList elements and styles

---
+++ src/components/ActionItemsList.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import { Avatar, ProgressBar } from '@shopify/polaris';
+import './ActionItemsList.css';
+import uncheckedImage from '../assets/checkbox-unchecked.png';
+import checkedImage from '../assets/checkbox-checked.png';
+
+const defaultDescription =
+  'Call your MP and tell them that you want them to vote yes on prop 27 because. Your MP is Julie Dabrusin';
+
+const ActionItemCheckBox = ({ status = false }) =>
+  status ? <img src={checkedImage} /> : <img src={uncheckedImage} />;
+
+const ActionItem = ({
+  title = 'Title',
+  description = defaultDescription,
+  status = false,
+  usersCompleted = 300,
+  usersTotal = 400
+}) => (
+  <div className="action-item">
+    <Avatar
+      className="action-item__avatar"
+      customer={false}
+      source="https://picsum.photos/240/240"
+      size="large"
+    />
+
+    <div className="action-item__content">
+      <div className="action-item__content__top">
+        <b className="action-item__title">{title}</b>
+        <div className="action-item__progress">
+          <ProgressBar
+            className="action-item__progress__bar"
+            progress={(usersCompleted / usersTotal) * 100}
+          />
+          <div className="action-item__progress__numbers">
+            <p
+            >{`${usersCompleted} out of ${usersTotal} followers have done this.`}</p>
+          </div>
+        </div>
+      </div>
+      <p className="action-item__description">{description}</p>
+    </div>
+
+    <ActionItemCheckBox status={status} />
+  </div>
+);
+
+class ActionItemsList extends Component {
+  render() {
+    return (
+      <div className="action-items-list">
+        <h2>Action Items</h2>
+        <div className="action-items-list">
+          <ActionItem status={true} usersCompleted={176} usersTotal={253} />
+          <ActionItem usersCompleted={234} usersTotal={253} />
+          <ActionItem usersCompleted={112} usersTotal={253} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ActionItemsList;
--- src/views/Solutions.js
+++ src/views/Solutions.js
@@ -2,13 +2,19 @@ import React, { Component } from 'react';
 import { Layout, Card, FormLayout, TextField, Button } from '@shopify/polaris';
 import TalkingPointList from '../components/TalkingPointList';
 import AddATalkingPoint from '../components/AddATalkingPoint';
+import ActionItemsList from '../components/ActionItemsList';
 import TagList from '../components/TagList';
 
 class Solutions extends Component {
   render() {
     return (
       <div>
-        <TagList tags={['housing', 'geography', 'safety', 'people']}/>
+        <TagList tags={['housing', 'geography', 'safety', 'people']} />
+        <Layout>
+          <Layout.Section>
+            <ActionItemsList />
+          </Layout.Section>
+        </Layout>
         <Layout>
           <Layout.Section secondary>
             <TalkingPointList />
