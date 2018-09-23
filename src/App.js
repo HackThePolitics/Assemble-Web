8711d3b13e56a46969ef73af100281c14a80f525
Jie <j42lin@users.noreply.github.com>
2018-09-22 22:18:39 -0400

    More styling

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 import { compose } from 'redux';
 import { Switch, Route } from 'react-router-dom';
+import { firestoreConnect } from 'react-redux-firebase';
 import Header from './components/Headers';
 import Home from './views/Home';
 import PostalCode from './views/PostalCode';
 import Problem from './views/Problem';
 import Test from './views/Test';
 import FirebaseTest from './views/FirebaseTest';
-import { firestoreConnect } from 'react-redux-firebase';
-import Problem from "./views/Problem";
 import SolutionsToTheProblem from "./views/SolutionsToTheProblem";
 
 import firebaseConfig from './config/firebase';
+
+import './App.css';
 
 class App extends Component {
   render() {
@@ -20,16 +21,18 @@ class App extends Component {
     }
 
     return (
-      <div className="App">
+      <div>
         <Header />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          {/* <Route path="/problems/:problemId/solutions" component={Solutions} /> */}
-          <Route path="/problem" component={Problem} />
-          <Route path="/postal" component={PostalCode} />
-          <Route path="/test" component={Test} />
-          <Route path="/firebase-test" component={FirebaseTest} />
-        </Switch>
+        <div className="app-container">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            {/* <Route path="/problems/:problemId/solutions" component={Solutions} /> */}
+            <Route path="/problem" component={Problem} />
+            <Route path="/postal" component={PostalCode} />
+            <Route path="/test" component={Test} />
+            <Route path="/firebase-test" component={FirebaseTest} />
+          </Switch>
+        </div>
       </div>
     );
   }
--- src/views/Test.js
+++ src/views/Test.js
@@ -5,7 +5,7 @@ class Test extends Component {
   render() {
     return (
       <div>
-        <h1>This is a test page for components</h1>
+        {/* <h1>This is a test page for components</h1> */}
         <SolutionsToTheProblem />
       </div>
     );
